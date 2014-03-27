341ad544e955741095beecbb0b3770420320e3be
Kyle Lewis <pseudobyte@gmail.com>
2014-03-27 01:33:32 -0600

    level loader

--- unlock/tile.js
+++ unlock/tile.js
@@ -1,13 +1,14 @@
 Crafty.c('Tile', {
 	init: function() {
-		this.addComponent('2D, Canvas, Color');
+		this.addComponent('2D, Canvas, Color, Mouse');
 		this.color('red');
 		this.attr({w: Game.geometry.tile.w, 
 				   h: Game.geometry.tile.h,
 				   x: 0,
 				   y: 0,
 				   z: 0});
-		this.glyph = Crafty.e('Glyph');
+				   
+		this.glyph = Crafty.e('Glyph, Mouse');
 		this.glyph.attr({w: Game.geometry.tile.w, 
 				   h: Game.geometry.tile.h,
 				   x: 0,
@@ -15,18 +16,31 @@ Crafty.c('Tile', {
 				   z: 1});
 		this.attach(this.glyph);
 		
-		this.mutable = true;
 		this.func = ' ';
 		this.group = 0;
+		
+		this.offX = 0;
+		this.offY = 0;
+		
+		this.mutable = true;
 		this.selected = false;
+		this.isEditor = 0;
+		this.index = null;
+		
+		var alsoThis = this;
+		this.glyph.bind('Click', function(e) {
+			if (e.mouseButton == Crafty.mouseButtons.LEFT) {
+				alsoThis.handleClick();
+			}
+		});
 	},
 	at: function(x, y) {
 		if (x === undefined || y === undefined) {
-			return { x: this.x/Game.geometry.tile.w - 1,
-			         y: this.y/Game.geometry.tile.h - 1};
+			return { x: this.x/Game.geometry.tile.w - this.offX,
+			         y: this.y/Game.geometry.tile.h - this.offY};
 		} else {
-			this.attr({ x: (x + 1) * Game.geometry.tile.w,
-			            y: (y + 1) * Game.geometry.tile.h});
+			this.attr({ x: (x + this.offX) * Game.geometry.tile.w,
+			            y: (y + this.offY) * Game.geometry.tile.h});
 			return this;
 		}
 	},
@@ -46,9 +60,25 @@ Crafty.c('Tile', {
 		if (this.selected != sel) {
 			this.selected = sel;
 			if (sel) {
-				this.attr({y: this.y - Game.geometry.tile.h / 2});
+				this.attr({y: this.y + Game.geometry.tile.h / 4});
 			} else {
-				this.attr({y: this.y + Game.geometry.tile.h / 2});
+				this.attr({y: this.y - Game.geometry.tile.h / 4});
+			}
+		}
+	},
+	handleClick: function() {
+		if (this.isEditor == 2 && !this.selected) {
+			Game.selectEdit(this.index);
+		} else if (this.isEditor == 1 && !this.selected) {
+			Game.setEditGroup(this.index);
+		} else if (!this.isEditor && this.mutable && Game.canEdit) {
+			if (Game.selGroup != null && Game.selEdit != null) {
+				var model = Game.editorTiles[Game.selEdit];
+				if (Game.selEdit == -1) {
+					model = Game.blankTile;
+				}
+				this.setGroup(model.group);
+				this.setFunc(model.func);
 			}
 		}
 	}
--- unlock/unlock.js
+++ unlock/unlock.js
@@ -8,9 +8,17 @@ Game = {
 		}
 	},
 	tiles: [],
-	groups: [{color: '#005869'},
-			 {color: '#00856A'}, 
-	         {color: '#8DB500'}],
+	groups: [{color: '#005869', funcs: ['#', '^', 'v', '<', '>', 'Y', 'X']},
+			 {color: '#00856A', funcs: ['#', '^', 'v', '<', '>', '++', '--', '<<', '>>',
+			                            '+', '-', '*', '/', '~']}, 
+	         {color: '#8DB500', funcs: ['#', '^', 'v', '<', '>', '++', '--', '<<', '>>',
+			                            '+', '-', '*', '/', '~']}],
+	blankTile: null,
+	editorTiles: [],
+	selGroup: null,
+	selEdit: null,
+	canEdit: true,
+	start: {x: null, y:null},
 	w: function() {
 		return (this.geometry.w + 2) * this.geometry.tile.w;
 	},
@@ -63,27 +71,119 @@ Game = {
 	init: function() {
 		Crafty.init(this.w(), this.h(), $('#game').get(0));
 		Crafty.background('#221133');
+		this.buildEditor();
 		
+		var level = {
+			offX: 0,
+			offY: 0,
+			data: [
+				'__________________',
+				'__________________',
+				'__________________',
+				'__________________',
+				'>_________________',
+				'__________________',
+				'__________________',
+				'__________________',
+				'__________________',
+				'  ________________'
+			]
+		};
+		this.buildLevel(level);
+	},
+	resetLevel: function() {
 		for (var j = 0; j < this.geometry.h; j++) {
-			this.tiles[j] = [];
+			if (!this.tiles[j]) {
+				this.tiles[j] = [];
+			}
 			for (var i = 0; i < this.geometry.w; i++) {
-				this.tiles[j][i] = Crafty.e('Tile')
-					.at(i, j);
-				this.tiles[j][i].setGroup(0);
+				if (this.tiles[j][i]) {
+					this.tiles[j][i].destroy();
+				}
+				this.tiles[j][i] = null;
 			}
 		}
-		this.buildEditor();
+		this.start.x = null;
+		this.start.y = null;
+	},
+	buildLevel: function(level) {
+		this.resetLevel();
+		for (var j = 0; j < level.data.length; j++) {
+			for (var i = 0; i < level.data[0].length; i++) {
+				if (level.data[j][i] == " ") {
+					
+				} else {
+					tile = Crafty.e('Tile');
+					tile.offX = 1 + level.offX;
+					tile.offY = 2 + level.offY;
+					tile.at(i, j);
+					tile.setGroup(0);
+					if (level.data[j][i] == "_") {
+					
+					} else {
+						tile.setFunc('.' + level.data[j][i]);
+						tile.mutable = false;
+					}
+					this.tiles[j + level.offY][i + level.offX] = tile;
+				}
+			}
+		}
+		this.level = level;
 	},
 	buildEditor: function() {
 		for (var i = 0; i < this.groups.length; i++) {
-			this.groups[i].tile = Crafty.e('Tile')
-				.at(i - 1, this.geometry.h + 1);
-			this.groups[i].tile.setGroup(i);
+			var tile = Crafty.e('Tile');
+			tile.at(i, 0);
+			tile.setGroup(i);
+			tile.isEditor = 1;
+			tile.index = i;
+			this.groups[i].tile = tile;
 		}
-		this.groups[0].tile.setSelected(true);
-		this.buildEditorGroup(0);
+		var tile = Crafty.e('Tile');
+		tile.setGroup(0);
+		tile.offX = this.groups.length + 1;
+		tile.at(0, 0);
+		tile.isEditor = 2;
+		tile.index = -1;
+		this.blankTile = tile;
+		this.setEditGroup(0);
+		this.selectEdit(-1);
 	},
-	buildEditorGroup: function(group) {
-		
+	setEditGroup: function(group) {
+		if (this.selGroup != null) {
+			this.groups[this.selGroup].tile.setSelected(false);
+			for (var i = 0; i < this.editorTiles.length; i++) {
+				this.editorTiles[i].destroy();
+			}
+			this.editorTiles = [];
+		}
+		if (this.selEdit != -1) {
+			this.selEdit = null;
+		}
+		this.selGroup = group;
+		this.groups[group].tile.setSelected(true);
+		for (var i = 0; i < this.groups[group].funcs.length; i++) {
+			var tile = Crafty.e('Tile');
+			tile.setFunc(this.groups[group].funcs[i]);
+			tile.setGroup(group);
+			tile.isEditor = 2;
+			tile.index = i;
+			tile.offX = this.groups.length + 2;
+			tile.at(i, 0);
+			this.editorTiles[i] = tile;
+		}
+	},
+	selectEdit: function(edit) {
+		if (this.selEdit == -1) {
+			this.blankTile.setSelected(false);
+		} else if (this.selEdit != null) {
+			this.editorTiles[this.selEdit].setSelected(false);
+		}
+		if (edit == -1) {
+			this.blankTile.setSelected(true);
+		} else {
+			this.editorTiles[edit].setSelected(true);
+		}
+		this.selEdit = edit;
 	}
 };
